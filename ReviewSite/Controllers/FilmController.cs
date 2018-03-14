69acd3eb27c72f89346d9c0055af87ccc5935b5a
Christopher Lucas (P16187512) <Chris.Lucas18@gmail.com>
2018-03-14 18:01:53 +0000

    Started Film Pages, Created 3 Test Pages

---
+++ ReviewSite/Controllers/FilmController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReviewSite_Views.Models;
+
+namespace ReviewSite.Controllers
+{
+    public class FilmController : Controller
+    {
+        private List<Film> _films = new List<Film>() {
+        new Film { FilmId = 1,
+                    Name = "Test 1",
+                    Description = "",
+                    Genre = "" },
+        new Film { FilmId = 2,
+                    Name = "Test 2",
+                    Description = "",
+                    Genre = "" },
+        new Film { FilmId = 3,
+                    Name = "Test 3",
+                    Description = "",
+                    Genre = "" },
+        };
+
+        // GET: Film
+        public ActionResult Index()
+        {
+            return View(_films);
+        }
+
+        // GET: Details/id
+        public ActionResult Details(int? id)
+        {
+            if (id == null) return new HttpNotFoundResult();
+
+            Film selectedFilm = _films.First(p => p.FilmId == id);
+
+            if (selectedFilm == null) return new HttpNotFoundResult();
+
+            return View(selectedFilm);
+        }
+
+    }
+}
