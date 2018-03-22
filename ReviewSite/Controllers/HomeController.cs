b257d440ceaac84616be3864a1aba262c7303837
Christopher Lucas (P16187512) <Chris.Lucas18@gmail.com>
2018-03-22 11:50:31 +0000

    Revert "MVC 14/3/18 Started Creating Pages, added Test Info"
    This reverts commit 77769f0d48182bd94ce9c5b7ea00aca74bc60fa5.

--- ReviewSite/Controllers/HomeController.cs
+++ ReviewSite/Controllers/HomeController.cs
@@ -10,20 +10,19 @@ namespace ReviewSite.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["Welcome!"] = "Welcome to this shit show";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
