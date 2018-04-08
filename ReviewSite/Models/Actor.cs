f624e505cd4c691d6d49dbdb16572a08bd675b5d
MattyJP <mjpery94@outlook.com>
2018-04-08 18:28:43 +0100

    Added Actor model, with Add, View and Edit functions. Add and Edit functions only visible to Admins.

---
+++ ReviewSite/Models/Actor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Actor
+    {
+        public virtual int ActorId { get; set; }
+        public virtual string Name { get; set; }
+        [Required]
+        //Display the date of birth in date format only
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public virtual DateTime DateOfBirth { get; set; }
+        [Required]
+        [Display(Name = "Actor Summmary")]
+        public virtual string ActorSummary { get; set; }
+    }
+}
